a1e12d5da4a081736dd4ba690e848412f66644e1
SashaTolkodubova <atolkodubova@gmail.com>
2023-03-14 16:31:36 +0100

    add code lesson5

---
+++ brain_games/even.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import prompt
+import random
+
+def even_game():
+    correct_answers = 0
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while correct_answers < 3:
+        random_number = random.randrange(1,99,1)
+        print(f'Question: {random_number}')
+        answer = prompt.string('Your answer: ')
+        if random_number % 2 == 0:
+            if answer == 'yes':
+                print("Correct!")
+                correct_answers += 1
+                continue
+            else:
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+                print(f"Let's try again, {name}!")
+                return
+
+        else:
+            if answer == 'no':
+                print("Correct!")
+                correct_answers += 1
+                continue
+            else:
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
+                print(f"Let's try again, {name}!")
+                return
+    print(f"Congratulations, {name}!")
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import prompt
+from brain_games import even
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    even.even_game()
+
+
+if __name__ == '__main__':
+    main()
+
